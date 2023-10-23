fdcdd154f73bfcbad8f917ec55b9a85865ba6b9d
disha4u <disha.97.agarwal@gmail.com>
2023-10-22 17:05:18 -0700

    first commit

--- handler.py
+++ handler.py
@@ -1,9 +1,15 @@
-from boto3 import client as boto3_client
+#from boto3 import client as boto3_client
 import face_recognition
 import pickle
+import os
+#from s3_utils import S3
 
 input_bucket = "546proj2"
 output_bucket = "546proj2output"
+
+
+# def download_video_file(key,path):
+# 	S3.read_file(input_bucket,key,path)
 
 # Function to read the 'encoding' file
 def open_encoding(filename):
@@ -11,6 +17,30 @@ def open_encoding(filename):
 	data = pickle.load(file)
 	file.close()
 	return data
+#(event, context):	
+def face_recognition_handler(im,data):
+    unknown_pic = face_recognition.load_image_file(im)
+    unknown_encoding = face_recognition.face_encodings(unknown_pic)[0]
+    for i,enc in enumerate(data["encoding"]):
+        results = face_recognition.compare_faces([enc], unknown_encoding)
+        if results[0] == True:
+            print("It's a picture of "+data["name"][i])
+            break
+        
+	
 
-def face_recognition_handler(event, context):	
-	print("Hello")
+def get_images(video_path):
+	path="./tmp/"
+	os.system("ffmpeg -i " + str(video_path) + " -r 1 " + str(path) + "image-%3d.jpeg")
+	return path+"image-001.jpeg"
+
+
+if __name__=="__main__":
+    d=open_encoding("encoding")
+    #print(d)
+    face_recognition_handler("./tmp/image-001.jpeg",d)
+    #vid="/home/disha4u/cloud/test_0.mp4"
+	#im=get_images(vid)
+	
+	
+
---
+++ s3_utils.py
@@ -0,0 +1,50 @@
+import boto3
+import os
+
+class S3(object):
+
+    aws_access_key_id = 'AKIA3G6GKCEDZRM2IIQO'
+    aws_secret_access_key = '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'
+    s3=boto3.client('s3',aws_access_key_id=aws_access_key_id,  aws_secret_access_key=aws_secret_access_key,region_name='us-east-1')
+
+
+    @staticmethod
+    def find_bucket(name):
+        all_buckets=S3.s3.list_buckets().get('Buckets')
+        for bucket in all_buckets:
+            print(bucket['Name'])
+            if bucket['Name']==name:
+                return True
+        return False
+    
+    @staticmethod
+    def create_bucket(name):
+        S3.s3.create_bucket(Bucket=name,CreateBucketConfiguration={'LocationConstraint': 'us-east-1a'})
+    
+    @staticmethod
+    def add_object(name,key,value):
+        S3.s3.put_object(Body=value,Bucket=name, Key=key)
+
+    @staticmethod
+    def upload_file(name,filename,filepath):
+
+        S3.s3.upload_file(Filename=filepath, Bucket=name,Key=filename)
+
+    @staticmethod
+    def read_file(name,key,download_path):
+        S3.s3.download_file(Bucket=name,Key=key,Filename=download_path)
+        
+    
+    @staticmethod
+    def read_object(name,key):
+        dc=S3.s3.get_object(Bucket=name,Key=key)
+        return dc['Body']
+
+    @staticmethod
+    def get_num_objects(name):
+        resp = S3.s3.list_objects_v2(Bucket=name)
+        count=0
+        for obj in resp['Contents']:
+            if obj['Size'] != 0:
+                count+=1
+        return count
